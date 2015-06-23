b425268cd65c0f8c3d2b294aa90bc4a55cb3d376
Marian Rudzynski <mr@impaled.org>
2015-06-23 22:08:21 +0200

    Add video upload support

--- lib/twitter/rest/request.rb
+++ lib/twitter/rest/request.rb
@@ -32,25 +32,30 @@ module Twitter
 
       # @return [Array, Hash]
       def perform
-        response = perform_raw
-        response_body = symbolize_keys!(response.parse)
+        options_key = @request_method == :get ? :params : :form
+        response = http_client.with(@headers).public_send(@request_method, @uri.to_s, options_key => @options)
+        response_body = response.body.empty? ? '' : symbolize_keys!(response.parse)
         response_headers = response.headers
         fail_or_return_response_body(response.code, response_body, response_headers)
       end
 
-      def perform_raw
-        options_key = @request_method == :get ? :params : :form
-        http_client.with(@headers).public_send(@request_method, @uri.to_s, options_key => @options)
+
+      def merge_multipart_file!(options)
+        key = options.delete(:key)
+        file = options.delete(:file)
+
+        if file.is_a?(StringIO)
+          options.merge!(key => HTTP::FormData::File.new(file, mime_type: 'video/mp4'))
+        else
+          options.merge!(key => HTTP::FormData::File.new(file, filename: File.basename(file), mime_type: mime_type(File.basename(file))))
+        end
       end
 
-    private
       def set_multipart_options!(request_method, options)
         if request_method == :multipart_post
-          key = options.delete(:key)
-          file = options.delete(:file)
+          merge_multipart_file!(options)
           @request_method = :post
-          @headers = Twitter::Headers.new(@client, @request_method, @uri, options).request_headers
-          options.merge!(key => HTTP::FormData::File.new(file, filename: File.basename(file), mime_type: mime_type(File.basename(file))))
+          @headers = Twitter::Headers.new(@client, @request_method, @uri).request_headers
         elsif request_method == :csv_post
           # TODO: This is a horrible hack. I'll figure out how to refactor/do correctly later.
           key = options.delete(:key)
